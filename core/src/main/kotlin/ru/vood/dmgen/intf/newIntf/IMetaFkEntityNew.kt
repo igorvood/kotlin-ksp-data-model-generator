bd391d005c455563118a35a6436e6e692f57fcfd
vood <balitsky_igor@list.ru>
2023-11-11 19:18:49 +0300

    init

--- core/src/main/kotlin/ru/vood/dmgen/intf/newIntf/IMetaFkEntityNew.kt
+++ core/src/main/kotlin/ru/vood/dmgen/intf/newIntf/IMetaFkEntityNew.kt
@@ -1,23 +1,24 @@
 package ru.vood.dmgen.intf.newIntf
 
 import ru.vood.dmgen.annotation.RelationType
+import ru.vood.dmgen.annotation.UkName
 import ru.vood.dmgen.intf.*
 
-interface IMetaFkEntityNew<T : IEntity<T>> {
-    val fromEntity: IMetaUkEntityNew<T>
-    val toEntity: IMetaUkEntityNew<T>
-    val uk: IMetaUkEntityNew<T>
+interface IMetaFkEntityNew/*<T : IEntity<T>>*/ {
+    val fromEntity: EntityName
+    val toEntity: EntityName
+    val uk: UkName
     val relationType: RelationType
     val fkCols: Set<FkPair>
 //    val contextOfClass: KClass<IContextOf<T>>
 }
 
-data class FKEntityData<T : IEntity<T>>(
-    override val fromEntity: IMetaUkEntityNew<T>,
-    override val toEntity: IMetaUkEntityNew<T>,
-    override val uk: IMetaUkEntityNew<T>,
+data class FKEntityData/*<T : IEntity<T>>*/(
+    override val fromEntity: EntityName,
+    override val toEntity: EntityName,
+    override val uk: UkName,
     override val relationType: RelationType,
     override val fkCols: Set<FkPair>
 //    override val contextOfClass: KClass<IContextOf<T>>
-) : IMetaFkEntityNew<T> {
+) : IMetaFkEntityNew/*<T>*/ {
 }
--- processor/src/main/kotlin/ru/vood/processor/datamodel/gen/meta/ForeignKeyMapGenerator.kt
+++ processor/src/main/kotlin/ru/vood/processor/datamodel/gen/meta/ForeignKeyMapGenerator.kt
@@ -2,11 +2,13 @@ package ru.vood.processor.datamodel.gen.meta
 
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
+import ru.vood.dmgen.annotation.FkName
 import ru.vood.dmgen.annotation.RelationType
-import ru.vood.dmgen.intf.FkPair
-import ru.vood.dmgen.intf.IMetaEntity
-import ru.vood.dmgen.intf.IMetaFkEntity
-import ru.vood.dmgen.intf.IMetaUkEntity
+import ru.vood.dmgen.annotation.UkName
+import ru.vood.dmgen.intf.*
+import ru.vood.dmgen.intf.newIntf.FKEntityData
+import ru.vood.dmgen.intf.newIntf.IMetaFkEntityNew
+import ru.vood.dmgen.intf.newIntf.IMetaUkEntityNew
 import ru.vood.processor.datamodel.abstraction.model.MetaInformation
 import ru.vood.processor.datamodel.gen.*
 import ru.vood.processor.datamodel.gen.meta.ColumnEntityEnumGenerator.Companion.columnEntityEnumGeneratorNameClass
@@ -45,13 +47,54 @@ class ForeignKeyMapGenerator(
                     .sorted()
                     .joinToString(",\n")
 
-                val trimIndent =
+                val entitiesMap = generatedClassData
+                    .map { metaForeign ->
+//                        "${ent.shortName}_${f.name.value}"
+                        val fkCols = metaForeign.fkCols.map { fkPa ->
+
+                            """FkPair(${metaForeign.fromEntity.shortName}_${fkPa.from.name.value}, ${metaForeign.toEntity.shortName}_${fkPa.to.name.value})"""
+                        }.joinToString(",\n")
+
+                        """FkName ("${metaForeign.name.value}") to FKEntityData(
+                        |EntityName("${metaForeign.fromEntity.shortName}"),
+                        |EntityName("${metaForeign.toEntity.shortName}"),
+                        |UkName("${metaForeign.uk.name.value}"),
+                        |${RelationType::class.java.canonicalName}.${metaForeign.relationType.name},
+                        |setOf($fkCols),
+                        |
+                        |)""".trimMargin()
+                    }
+                    .sorted()
+                    .joinToString(",\n")
+
+//                data class FKEntityData<T : IEntity<T>>(
+//                    override val fromEntity: IMetaUkEntityNew<T>,
+//                    override val toEntity: IMetaUkEntityNew<T>,
+//                    override val uk: IMetaUkEntityNew<T>,
+//                    override val relationType: RelationType,
+//                    override val fkCols: Set<FkPair>
+////    override val contextOfClass: KClass<IContextOf<T>>
+//                ) : IMetaFkEntityNew<T> {
+
+
+                    val trimIndent =
                     """package ${packageName.value}
                         
 import ${packageName.value}.${EntityEnumGenerator.nameClassEntityEnumGenerator}.*
 import ${packageName.value}.${UniqueKeyEnumGenerator.uniqueKeyEnumGeneratorNameClass}.*
 import ${FkPair::class.java.canonicalName}
+import ${FkName::class.java.canonicalName}
+import ${FKEntityData::class.java.canonicalName}
+import ${EntityName::class.java.canonicalName}
+import ${UkName::class.java.canonicalName}
 import ${packageName.value}.${columnEntityEnumGeneratorNameClass}.*
+
+
+val foreignKeyMap = mapOf(
+$entitiesMap
+
+)
+
 
 enum class $nameClass(
     override val fromEntity: ${IMetaEntity::class.java.canonicalName},
