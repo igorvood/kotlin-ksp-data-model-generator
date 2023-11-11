8af75c9551ffa38c32ba71a42d043f5b4eb63327
vood <balitsky_igor@list.ru>
2023-11-11 20:47:21 +0300

    init

--- workload/src/main/kotlin/ru/vood/dmgen/run/RunEntitySave.kt
+++ workload/src/main/kotlin/ru/vood/dmgen/run/RunEntitySave.kt
@@ -4,13 +4,15 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.jdbc.core.JdbcOperations
 import org.springframework.stereotype.Service
 import ru.vood.dmgen.dao.EntityDao
+import ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity
 import ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity
 import ru.vood.dmgen.datamodel.runtime.dataclasses.context.DealParamOneToOne_PKContext
 import ru.vood.dmgen.datamodel.runtime.dataclasses.context.DealParamOneToOne_UKContext
+import ru.vood.dmgen.datamodel.runtime.dataclasses.context.Deal_PKContext
 import ru.vood.dmgen.datamodel.valueClasses.DealId
 
 @Service
-class Run(
+class RunEntitySave(
     val entity: EntityDao,
     val jdbcOperations: JdbcOperations
 ) : CommandLineRunner {
@@ -19,13 +21,20 @@ class Run(
 
         val dealId = DealId("12")
         val paramDate = "asd"
-        val entity1 = DealParamOneToOneEntity(dealId, paramDate)
-        entity.saveEntity(entity1)
+        val dealParamOneToOneEntity = DealParamOneToOneEntity(dealId, paramDate)
+        entity.saveEntity(dealParamOneToOneEntity)
 
         val findByUk = entity.findByUk(DealParamOneToOne_PKContext(dealId))
         println(findByUk)
         val findByUk2 =
             entity.findByUk<DealParamOneToOneEntity>(DealParamOneToOne_UKContext(dealId, paramDate))
         println(findByUk2)
+
+        entity.saveEntity(DealEntity(dealId, "asd", null, true, null, dealParamOneToOneEntity, null, setOf()))
+
+        val findByUk1 = entity.findByUk(Deal_PKContext(dealId))
+
+        println(findByUk1)
+
     }
 }
