bbb22093fb881449ad63d989be6336d847381777
vood <balitsky_igor@list.ru>
2023-11-17 23:30:51 +0300

    init

--- core/src/main/kotlin/ru/vood/dmgen/intf/newIntf/IMetaColumnEntityNew.kt
+++ core/src/main/kotlin/ru/vood/dmgen/intf/newIntf/IMetaColumnEntityNew.kt
@@ -1,8 +1,8 @@
 package ru.vood.dmgen.intf.newIntf
 
-import ru.vood.dmgen.intf.SimpleColumnName
 import ru.vood.dmgen.intf.EntityName
 import ru.vood.dmgen.intf.IEntity
+import ru.vood.dmgen.intf.SimpleColumnName
 import kotlin.reflect.KProperty1
 
 interface IMetaColumnEntityNew<T : IEntity<out T>> {
@@ -24,8 +24,22 @@ data class ColumnEntityData<T : IEntity<out T>>(
 ) : IMetaColumnEntityNew<T>
 
 
-enum class ColumnKind{
-    SIMPLE,
-    SYNTHETIC,
-    SYNTHETIC_SET,
- }
+enum class ColumnKind {
+    SIMPLE, SYNTHETIC, SYNTHETIC_SET,
+}
+
+sealed interface IColKind<T : IEntity<out T>, OUT> {
+    val extractFieldValue: (entity: T) -> OUT
+}
+
+data class Simple<T : IEntity<out T>, OUT>(
+    override val extractFieldValue: (entity: T) -> OUT
+) : IColKind<T, OUT>
+
+data class Synthetic<T : IEntity<out T>, OUT : IEntity<out OUT>>(
+    override val extractFieldValue: (entity: T) -> OUT
+) : IColKind<T, OUT>
+
+data class SyntheticSet<T : IEntity<out T>, OUT : IEntity<out OUT>>(
+    override val extractFieldValue: (entity: T) -> Set<OUT>
+) : IColKind<T, Set<OUT>>
