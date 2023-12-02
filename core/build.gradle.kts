f391b093971ef0ceb0de1b034e7de13ab3255ed4
vood <balitsky_igor@list.ru>
2023-12-02 18:32:16 +0300

    init

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib"))
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-metadata-jvm
-    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.0")
 //    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 //    compile 'org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.0.3'
