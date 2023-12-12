667757c157394cf2df13113a5d26711be79c7155
vood <balitsky_igor@list.ru>
2023-12-12 21:41:05 +0300

    init

--- workload/src/main/kotlin/ru/vood/dmgen/meta/IndexesMetaDto.kt
+++ workload/src/main/kotlin/ru/vood/dmgen/meta/IndexesMetaDto.kt
@@ -3,10 +3,14 @@ package ru.vood.dmgen.meta
 import ru.vood.dmgen.intf.IEntityOrigin
 import ru.vood.dmgen.intf.newIntf.UKEntityData
 
+/**Мета информаци по уникальным индексам и первичным ключам*/
 data class IndexesMetaDto(
+    /**Мета по первичному ключу*/
     val pkEntityData: UKEntityData<out IEntityOrigin>,
+    /**мета по уникальным индексам, исключая первичный ключ*/
     val ukOnlySet: Set<UKEntityData<out IEntityOrigin>>
 ){
+    /**мета по уникальным индексам включая первичный ключ*/
     val ukAndPkMap = ukOnlySet.plus(pkEntityData).associateBy { it.ukName }
 
 }
