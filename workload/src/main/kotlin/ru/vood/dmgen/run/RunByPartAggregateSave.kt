10dd2bdc6401727a42c250f8846f2a092bcb5b77
vood <balitsky_igor@list.ru>
2024-01-02 19:44:19 +0300

    init

--- workload/src/main/kotlin/ru/vood/dmgen/run/RunByPartAggregateSave.kt
+++ workload/src/main/kotlin/ru/vood/dmgen/run/RunByPartAggregateSave.kt
@@ -53,7 +53,7 @@ class RunByPartAggregateSave(
         entity.saveAggregateByPart(dealExtendDataEntity)
 
 
-        val findByUk1 = entity.findSyntheticEntityCollectPartByUk(Deal_PKContext(dealId))
+        val findByUk1 = entity.findSyntheticEntityCollectPartByUk<DealEntity>(Deal_PKContext(dealId))
 
         log.info(aggregate.toString())
         log.info(findByUk1.toString())
