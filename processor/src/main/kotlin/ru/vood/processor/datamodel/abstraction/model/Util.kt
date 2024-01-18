220c4630b2bc0b6905189675939a7a53c6420821
vood <balitsky_igor@list.ru>
2024-01-18 19:10:23 +0300

    init

---
+++ processor/src/main/kotlin/ru/vood/processor/datamodel/abstraction/model/Util.kt
@@ -0,0 +1,14 @@
+package ru.vood.processor.datamodel.abstraction.model
+
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.symbol.KSNode
+
+fun KSPLogger.kspError(message: String, symbol: KSNode? = null){
+    this.error(message, symbol)
+    throw java.lang.IllegalStateException(message)
+}
+
+//fun logging(message: String, symbol: KSNode? = null)
+//fun info(message: String, symbol: KSNode? = null)
+//fun warn(message: String, symbol: KSNode? = null)
+//fun error(message: String, symbol: KSNode? = null)
