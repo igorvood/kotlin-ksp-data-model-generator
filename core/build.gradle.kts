46390c763003a78526d27a84aef19f041d8db1fd
vood <balitsky_igor@list.ru>
2024-01-23 21:16:18 +0300

    init

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,13 +7,7 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-metadata-jvm
-    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.0")
-//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-//    compile 'org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.0.3'
-//    implementation("com.sun.java:tools:11.0.1")
-    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.1")
 }
 
 repositories {
