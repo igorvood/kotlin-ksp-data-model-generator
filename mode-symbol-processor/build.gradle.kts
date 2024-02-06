6083731fe57fe706148b01b80030cb7c854ae264
vood <balitsky_igor@list.ru>
2024-02-06 18:45:44 +0300

    init

--- mode-symbol-processor/build.gradle.kts
+++ mode-symbol-processor/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("com.charleskorn.kaml:kaml:0.48.0")
+    implementation("com.charleskorn.kaml:kaml:0.52.0")
     implementation("com.squareup:kotlinpoet:1.12.0")
     implementation("com.squareup:kotlinpoet-ksp:1.12.0")
 
