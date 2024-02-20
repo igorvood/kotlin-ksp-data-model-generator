c902bc2771f43849b857ea44f75558d5fde445ec
vood <balitsky_igor@list.ru>
2024-02-20 20:00:07 +0300

    init

--- mode-symbol-processor/src/test/kotlin/ru/vood/processor/datamodel/generator/common/ColumnEntityDataSingleFileGeneratorTest.kt
+++ mode-symbol-processor/src/test/kotlin/ru/vood/processor/datamodel/generator/common/ColumnEntityDataSingleFileGeneratorTest.kt
@@ -1,29 +1,48 @@
 package ru.vood.processor.datamodel.generator.common
 
+import com.squareup.kotlinpoet.FileSpec
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import ru.vood.model.generator.ksp.common.dto.PackageName
 import ru.vood.processor.datamodel.abstraction.AbstractGeneratorTest
+import ru.vood.processor.datamodel.generator.util.readFile
 
 internal class ColumnEntityDataSingleFileGeneratorTest : AbstractGeneratorTest("DataModel.json") {
 
+    lateinit var generatedFiles: List<FileSpec>
 
-    @Test
-    fun textFileTest() {
-
+    @BeforeAll
+    fun asd(){
         val columnEntityDataSingleFileGenerator = ColumnEntityDataSingleFileGenerator(PackageName("test"))
+        generatedFiles = columnEntityDataSingleFileGenerator.files()
+    }
 
-        val files = columnEntityDataSingleFileGenerator.files()
+    @ParameterizedTest
+    @MethodSource("ru.vood.processor.datamodel.generator.common.ColumnEntityDataSingleFileGeneratorTest#testCaseData")
+    fun textFileTest(testCase: TestCase) {
+
+        val files: List<FileSpec> = generatedFiles
+            .filter { it.name == testCase.expectedClassFile }
+
+
         assertEquals(1, files.size)
 
         val fileSpec = files[0]
 
-        assertEquals(s, fileSpec.toJavaFileObject().getCharContent(false))
+        assertEquals(testCase.getText(), fileSpec.toJavaFileObject().getCharContent(false))
 
     }
 
     companion object {
+        private val testData = listOf(TestCase("ColumnEntityData"))
+
+        @JvmStatic
+        private fun testCaseData() = testData.map{Arguments.of(it)}
+
+
         val s = """package test.intf
 
 import javax.`annotation`.processing.Generated
@@ -59,6 +78,12 @@ public sealed interface ColumnEntityData {
   public val comment: String
 }
 """
+
+    }
+
+    data class TestCase(val expectedClassFile: String) {
+
+        fun getText() = this::class.java.readFile(expectedClassFile)
 
     }
 }
