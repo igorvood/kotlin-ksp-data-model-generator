4f5b27244508d61e5c03f32a95261a507d910d8b
vood <balitsky_igor@list.ru>
2024-02-21 19:26:49 +0300

    init

--- mode-symbol-processor/src/test/kotlin/ru/vood/processor/datamodel/DataModelConfigProcessorProviderTest.kt
+++ mode-symbol-processor/src/test/kotlin/ru/vood/processor/datamodel/DataModelConfigProcessorProviderTest.kt
@@ -3,8 +3,10 @@ package ru.vood.processor.datamodel
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import ru.vood.processor.datamodel.abstraction.AbstractKoraAppProcessorTest
 
+@Disabled
 internal class DataModelConfigProcessorProviderTest: AbstractKoraAppProcessorTest() {
 
     @Test
