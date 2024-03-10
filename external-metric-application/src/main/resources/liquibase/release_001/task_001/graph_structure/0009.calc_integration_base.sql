5809a70c974651cacde73e2c956b1ab1667d99bf
vood <balitsky_igor@list.ru>
2024-03-10 18:44:07 +0300

    init

--- external-metric-application/src/main/resources/liquibase/release_001/task_001/graph_structure/0009.calc_integration_base.sql
+++ external-metric-application/src/main/resources/liquibase/release_001/task_001/graph_structure/0009.calc_integration_base.sql
@@ -7,9 +7,9 @@ create table calc_integration_base
 --     node_type varchar(256) generated always as ('topic') stored,
     request_id varchar(256) generated always as (id) stored,
     constraint calc_integration_base_request_id unique (business_type_call, request_id),
-    response_id varchar(256),
-    constraint calc_integration_base_response_id unique (business_type_call, response_id),
-    is_finished bool generated always as (response_id!=null) stored
+    last_response_id varchar(256),
+    constraint calc_integration_base_response_id unique (business_type_call, last_response_id),
+    is_finished bool generated always as (last_response_id!=null) stored
 )
 /
 comment on table calc_integration_base is 'базовая таблица регистрации интеграций.'
@@ -20,7 +20,7 @@ comment on column calc_integration_base.business_type_call is 'Бизнесс т
 /
 comment on column calc_integration_base.request_id is 'Идентификатор запроса .'
 /
-comment on column calc_integration_base.response_id is 'Идентификатор ответа.'
+comment on column calc_integration_base.last_response_id is 'Идентификатор последнего ответа.'
 /
 comment on column calc_integration_base.is_finished is 'Признак завершенности.'
 /
