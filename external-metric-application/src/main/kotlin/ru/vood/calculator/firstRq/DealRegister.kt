0b7aa29b3ec926d64b8226152d0474d462811de2
vood <balitsky_igor@list.ru>
2024-03-16 20:40:45 +0300

    init

--- external-metric-application/src/main/kotlin/ru/vood/calculator/firstRq/DealRegister.kt
+++ external-metric-application/src/main/kotlin/ru/vood/calculator/firstRq/DealRegister.kt
@@ -1,19 +1,22 @@
 package ru.vood.calculator.firstRq
 
+import org.springframework.stereotype.Service
 import ru.vood.calculator.ext.IntegrationRegistrar
 import ru.vood.calculator.ext.RequestData
 import ru.vood.calculator.ext.RequestService
 import ru.vood.calculator.ext.ResponseData
 import ru.vood.calculator.ext.meta.BusinessTypeCall
 
-class DealRegister: RequestService {
+@Service
+class DealRegister(
+    override val integrationInRegistrar: IntegrationRegistrar
+    ): RequestService {
     override val businessTypeCall: BusinessTypeCall
-        get() = TODO("Not yet implemented")
+        get() = BusinessTypeCall.START_CALCULATION
 
     override fun processIntegration(requestData: RequestData): ResponseData {
         TODO("Not yet implemented")
     }
 
-    override val integrationInRegistrar: IntegrationRegistrar
-        get() = TODO("Not yet implemented")
+
 }
