bc4eacdaae6633496bc8e06b0899bc26da1139be
vood <balitsky_igor@list.ru>
2024-04-03 18:36:32 +0300

    init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,10 @@
 //плугин с кодо генерацией https://habr.com/ru/companies/yota/articles/565440/
 //Kotlin Symbol Processing. От теории до практики https://habr.com/ru/companies/cian/articles/705278/
 // пример генератора от тинькова https://github.com/Tinkoff/kora?ysclid=ltojwtlwp7423328427
+//вроде оф сайт kotlin poet https://square.github.io/kotlinpoet/
+// в их гит хабе есть дока, на самом сайте ей маловато
+// https://github.com/square/kotlinpoet/tree/main/docs
+
 pluginManagement {
     val kotlinVersion: String by settings
     val kspVersion: String by settings
