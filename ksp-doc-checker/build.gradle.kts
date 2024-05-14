a9b840db37def6569ff986d1a82d24088fa8f820
vood <igor-vood@gmail.com>
2024-05-14 19:14:27 +0300

    init

--- ksp-doc-checker/build.gradle.kts
+++ ksp-doc-checker/build.gradle.kts
@@ -1,74 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val kspVersion: String by project
-val junitJupiter: String by project
-val kotlinCompileTesting: String by project
-val ioMockk: String by project
+val kotlinxSerializationVersion: String by project
+val kotlinpoet: String by project
 val kotlinVersion: String by project
 val kotlinxCoroutines: String by project
-val kotlinpoet: String by project
+val ioMockk: String by project
+val junitJupiter: String by project
+val kotlinCompileTesting: String by project
+
 
 plugins {
     kotlin("jvm")
-    `maven-publish`
     kotlin("plugin.serialization")
-//    id("maven-publish")
 }
 
-group = "ru.vood.ksp"
-version = "1.0-SNAPSHOT"
+repositories {
+    mavenCentral()
+}
 
 dependencies {
-
+    implementation(project(":core"))
+    implementation(project(":symbol-processor-common"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutines")
     implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
     implementation("com.google.devtools.ksp:symbol-processing:$kspVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+    implementation("com.charleskorn.kaml:kaml:0.53.0")
+//    implementation("com.charleskorn.kaml:kaml:0.52.0")
     implementation("com.squareup:kotlinpoet:$kotlinpoet")
     implementation("com.squareup:kotlinpoet-ksp:$kotlinpoet")
-
-    implementation("com.charleskorn.kaml:kaml:0.53.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
-
-
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiter")
     testImplementation("io.mockk:mockk:$ioMockk")
     testImplementation("com.github.tschuchortdev:kotlin-compile-testing:$kotlinCompileTesting")
     testImplementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
+//    testImplementation()
 
-}
-repositories {
-    mavenCentral()
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+val javaVersion: String by project
+tasks.withType<KotlinCompile>{
+    kotlinOptions{
+        jvmTarget = javaVersion
+    }
+}
+
 kotlin {
-//    sourceSets.main {
-//        kotlin.srcDir("build/generated/ksp/main/kotlin")
-//    }
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
     sourceSets.test {
         kotlin.srcDir("src/test/kotlin")
     }
 }
 
-publishing{
-    publications{
-        create<MavenPublication>("maven") {
-            groupId = "ru.vood.ksp"
-            artifactId = "ksp-doc-checker"
-            version = "1.1"
-
-            from(components["java"])
-        }
-    }
-}
-//publishing {
-//    publications {
-//        maven(MavenPublication) {
-//            groupId 'org.gradle.sample'
-//            artifactId 'project1-sample'
-//            version '1.1'
-//            from components.java
-//        }
-//    }
-//}
